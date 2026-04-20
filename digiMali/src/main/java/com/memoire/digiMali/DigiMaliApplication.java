5820104fc650bf35f88fbf5de1082ccfe725afbc
TimaaSiss <fatoumataasissoko09@gmail.com>
2026-04-20 17:44:55 +0200

    First step

---
+++ digiMali/src/main/java/com/memoire/digiMali/DigiMaliApplication.java
@@ -0,0 +1,13 @@
+package com.memoire.digiMali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiMaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiMaliApplication.class, args);
+	}
+
+}
---
+++ digiMali/src/test/java/com/memoire/digiMali/DigiMaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memoire.digiMali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiMaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
